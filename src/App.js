99099787ae1e3e5b52d4be0fd0460b9a7986bdd5
dbmarch <dbmarch@gmail.com>
2018-05-16 23:06:47 -0500

    example with create element.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      <p>Hi I am a react App! </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //   <p>Hi I am a react App! </p>
+    //   </div>
+    // );
+    return React.createElement('div', {className: 'App'}, React.createElement ('h1', null, 'Do you see my Text?'));
   }
 }
 
