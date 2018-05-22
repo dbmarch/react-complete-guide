04023cd59b0b7941767bfeb28c8d4c51e9408f22
dbmarch <dbmarch@gmail.com>
2018-05-21 23:13:53 -0500

    Started styling.  Added Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
+import Radium from 'radium';
 
 class App extends Component {
 
@@ -43,11 +44,16 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      borderRadius: '10px'
+      borderRadius: '10px',
+      ':hover':   {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
     let persons = null;
     if (this.state.showPersons) {
@@ -63,10 +69,26 @@ class App extends Component {
           })}
         </div>
        );
+       style.backgroundColor="red";
+       style[':hover'] = { 
+         backgroundColor: 'salmon',
+         color: 'black'
+       };
     }
+
+    //let classes = ['red', 'bold'].join(' ');
+    const classes = [];
+    if (this.state.persons.length <=2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <=1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
       <h1>Hi I am a react App! </h1>
+      <p className = {classes.join(' ')} > This is working! </p>
       <button style = {style} 
               onClick= {this.togglePersonsHandler}>Switch Name</button>
       {persons}      
@@ -75,4 +97,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
