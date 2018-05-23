886282058cfc04880ccfc5ca3478a4f29bfcf786
dbmarch <dbmarch@gmail.com>
2018-05-22 21:24:42 -0500

    Added R adium

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
 
@@ -86,6 +86,7 @@ class App extends Component {
     }
 
     return (
+      <StyleRoot>
       <div className="App">
       <h1>Hi I am a react App! </h1>
       <p className = {classes.join(' ')} > This is working! </p>
@@ -93,6 +94,7 @@ class App extends Component {
               onClick= {this.togglePersonsHandler}>Switch Name</button>
       {persons}      
       </div>
+      </StyleRoot>
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './Person.css';
 
 const person = ( props ) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
+
     return (
-        <div className = 'Person'>
+        <div className = 'Person' style = {style}>
             <p onClick={props.click}> I'm {props.name} and I am { props.age} years old ! </p>
             <p> {props.children}</p>
             <input type='text' onChange={props.changed} value={props.name}/>
@@ -22,4 +29,4 @@ class Person extends Component {
     }
 }
 
-export default  person;
+export default  Radium(person);
