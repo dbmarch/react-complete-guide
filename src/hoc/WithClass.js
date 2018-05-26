5faaf033fd5c617caa29e63e0ff25e062a2fb91e
dbmarch <dbmarch@gmail.com>
2018-05-25 21:49:25 -0500

    different types of hoc

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
-const withClass = (props) => (
+const withClass = (WrappedComponent, className) => {
+    return (props) => (
+        <div className= {className}>
+            <WrappedComponent {...props}/>
+        </div>
+    )
+}
+
+
+
+// Altnernate approach:
+const withClass2 = (WrappedComponent, className) => {
+    return class extends Component {
+        render() {
+            return (
+
+                <div className= {className}>
+                   <WrappedComponent {...props}/>
+               </div>
+            )
+        }
+    }
+}
+
+
+
+
+const WithClass = (props) => (
     <div className = {props.classes}>
        {props.children}
     </div>
 );
+export {withClass};
 
-export default withClass;
+export default WithClass;
+
