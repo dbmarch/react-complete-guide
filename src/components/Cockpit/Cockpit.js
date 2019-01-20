50b8c2ea74d8a4670d7688851bc1f927ce658580
dbmarch <dbmarch@gmail.com>
2019-01-20 11:59:41 -0600

    rename aux.js...Windows has an issue with it.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import classes from './Cockpit.css';
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/_Aux';
 
-const cockpit = (props) => {
-    const assignedClasses = [];
-    let btnClass = '';
+const cockpit = props => {
+	const assignedClasses = [];
+	let btnClass = '';
 
-    if (props.showPersons){
-        btnClass = classes.Red;
-    }
+	if (props.showPersons) {
+		btnClass = classes.Red;
+	}
 
-    if (props.persons.length <=2) {
-      assignedClasses.push(classes.red);
-    }
-    if (props.persons.length <=1) {
-      assignedClasses.push(classes.bold);
-    }
+	if (props.persons.length <= 2) {
+		assignedClasses.push(classes.red);
+	}
+	if (props.persons.length <= 1) {
+		assignedClasses.push(classes.bold);
+	}
 
-    return (
-
-        <Aux>
-        <div className={classes.Cockpit}>  
-        <h1>{ props.appTitle }</h1>
-        <h1>Hi I am a react App! </h1>
-        <p className = {assignedClasses.join(' ')} > This is working! </p>
-        <button className={btnClass}
-                onClick= {props.clicked}>Switch Name</button> 
-        <button onClick = {props.login}> Log In </button>
-        </div>
-        </Aux>
-      );
-}
+	return (
+		<Aux>
+			<div className={classes.Cockpit}>
+				<h1>{props.appTitle}</h1>
+				<h1>Hi I am a react App! </h1>
+				<p className={assignedClasses.join(' ')}> This is working! </p>
+				<button className={btnClass} onClick={props.clicked}>
+					Switch Name
+				</button>
+				<button onClick={props.login}> Log In </button>
+			</div>
+		</Aux>
+	);
+};
 
 export default cockpit;
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,61 +1,63 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
-import {withClass} from '../../../hoc/WithClass';
-import Aux from '../../../hoc/Aux';
+import { withClass } from '../../../hoc/WithClass';
+import Aux from '../../../hoc/_Aux';
 import PropTypes from 'prop-types';
-import {AuthContext} from '../../../containers/App';
+import { AuthContext } from '../../../containers/App';
 
 class Person extends Component {
-    constructor (props) {
-        super(props);
-        console.log ('[Person.js] constructor');
-        this.inputElement = React.createRef();
-    }
+	constructor(props) {
+		super(props);
+		console.log('[Person.js] constructor');
+		this.inputElement = React.createRef();
+	}
 
-    // Discouraged:
-    componentWillMount() {
-        console.log('[Person.js] ComponentWillMount');
-      }
-    
-      componentDidMount() {
-        console.log ('[Person.js] ComponentDidMount');
-        if (this.props.position === 0) {
-            //this.inputElement.focus();  // old way
-            this.inputElement.current.focus();
-        }
-      }
-  
-    focus() {
-        this.inputElement.current.focus();
-     }
+	// Discouraged:
+	componentWillMount() {
+		console.log('[Person.js] ComponentWillMount');
+	}
 
-    render(){
-        console.log ('[Person.js] render');
-                //{this.props.authenticated ? <p>AUTH</p> : <p> NOT_AUTH </p>}
-       
-        return (
-              <Aux>
-                <AuthContext.Consumer>
-                { auth => auth ? <p>AUTH</p> : <p> NOT_AUTH </p>}
-                </AuthContext.Consumer>
-                <p onClick={this.props.click}> I'm {this.props.name} and I am { this.props.age} years old ! </p>
-                <p> {this.props.children}</p>
-                <input 
-                    //ref={(inp) => { this.inputElement = inp}}
-                    ref = {this.inputElement}
-                    type='text' 
-                    onChange={this.props.changed} 
-                    value={this.props.name}/>
-            </Aux>
-        )
-    }
+	componentDidMount() {
+		console.log('[Person.js] ComponentDidMount');
+		if (this.props.position === 0) {
+			//this.inputElement.focus();  // old way
+			this.inputElement.current.focus();
+		}
+	}
+
+	focus() {
+		this.inputElement.current.focus();
+	}
+
+	render() {
+		console.log('[Person.js] render');
+		//{this.props.authenticated ? <p>AUTH</p> : <p> NOT_AUTH </p>}
+
+		return (
+			<Aux>
+				<AuthContext.Consumer>{auth => (auth ? <p>AUTH</p> : <p> NOT_AUTH </p>)}</AuthContext.Consumer>
+				<p onClick={this.props.click}>
+					{' '}
+					I'm {this.props.name} and I am {this.props.age} years old !{' '}
+				</p>
+				<p> {this.props.children}</p>
+				<input
+					//ref={(inp) => { this.inputElement = inp}}
+					ref={this.inputElement}
+					type="text"
+					onChange={this.props.changed}
+					value={this.props.name}
+				/>
+			</Aux>
+		);
+	}
 }
 
 Person.propTypes = {
-    click: PropTypes.func,
-    name:  PropTypes.string, 
-    age:   PropTypes.number,
-    changed: PropTypes.func
+	click: PropTypes.func,
+	name: PropTypes.string,
+	age: PropTypes.number,
+	changed: PropTypes.func,
 };
 
 export default withClass(Person, classes.Person);
