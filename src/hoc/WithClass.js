20cedb6391b3c1573e288d1e932be7492f88b78d
dbmarch <dbmarch@gmail.com>
2019-01-20 12:29:46 -0600

    rename aux

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,46 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 const withClass = (WrappedComponent, className) => {
-    const WithClassPlus = class extends Component {
-        render () {
-            return (
-            <div className= {className}>
-                <WrappedComponent ref={this.props.forwardedRef} {...this.props}/>
-            </div>
-            )
-        }
-    
-    }
+	const WithClassPlus = class extends Component {
+		render() {
+			return (
+				<div className={className}>
+					<WrappedComponent ref={this.props.forwardedRef} {...this.props} />
+				</div>
+			);
+		}
+	};
 
-    return React.forwardRef ((props,ref) => {
-        return <WithClassPlus {...props} forwardedRef = {ref} />
-    })
-}
+	return React.forwardRef((props, ref) => {
+		return <WithClassPlus {...props} forwardedRef={ref} />;
+	});
+};
 
+// // Altnernate approach:
+// const withClass2 = (WrappedComponent, className) => {
+//     return class extends Component {
+//         render() {
+//             return (
+//                 <div className= {className}>
+//                    <WrappedComponent ref = {this.props.forwardedRef} {...this.props}/>
+//                </div>
+//             )
+//         }
+//     }
+// }
 
-
-// Altnernate approach:
-const withClass2 = (WrappedComponent, className) => {
-    return class extends Component {
-        render() {
-            return (
-                <div className= {className}>
-                   <WrappedComponent ref = {this.props.forwardedRef} {...this.props}/>
-               </div>
-            )
-        }
-    }
-}
-
-
-
-
-const WithClass = (props) => (
-    <div className = {props.classes}>
-       {props.children}
-    </div>
-);
-export {withClass};
+const WithClass = props => <div className={props.classes}>{props.children}</div>;
+export { withClass };
 
 export default WithClass;
-
