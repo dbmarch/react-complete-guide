46a1d9f28912a311196a046a5b9a95a51b418f3b
dbmarch <dbmarch@gmail.com>
2019-01-23 21:58:51 -0600

    fix gitignore

--- Redux-01/src/store/reducer.js
+++ Redux-01/src/store/reducer.js
@@ -1,11 +1,32 @@
+import * as actionTypes from './actions';
+
 const initialState = {
 	counter: 0,
+	results: [],
 };
 
 const reducer = (state = initialState, action) => {
-	if (action.type === 'INCREMENT') return { ...state, counter: state.counter + 1 };
+	switch (action.type) {
+		case actionTypes.INCREMENT:
+			return { ...state, counter: state.counter + 1 };
+		case actionTypes.DECREMENT:
+			return { ...state, counter: state.counter - 1 };
+		case actionTypes.ADD:
+			return { ...state, counter: state.counter + action.value };
+		case actionTypes.SUBTRACT:
+			return { ...state, counter: state.counter - action.value };
+		case actionTypes.STORE_RESULT:
+			return { ...state, results: [...state.results, state.counter] };
+		case actionTypes.DELETE_RESULT:
+			console.log(action);
+			let newResults = state.results.slice(0);
+			newResults.splice(action.index, 1);
+			return { ...state, results: newResults };
 
-	return state;
+		default:
+			console.log('uncaught action in reducer ', action.type);
+			return state;
+	}
 };
 
 export default reducer;
