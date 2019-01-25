3a510d03d861f1711b3df2e1110f056a6252c6c1
dbmarch <dbmarch@gmail.com>
2019-01-24 21:32:46 -0600

    add input and age fields

--- redux--assignment-2/src/components/AddPerson/AddPerson.js
+++ redux--assignment-2/src/components/AddPerson/AddPerson.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+class AddPerson extends Component {
+	state = {
+		name: '',
+		age: null,
+	};
 
-export default addPerson;
+	nameChangedHandler = event => {
+		this.setState({ name: event.target.value });
+	};
+
+	ageChangedHandler = event => {
+		this.setState({ age: event.target.value });
+	};
+
+	render(props) {
+		return (
+			<div className="AddPerson">
+				<input type="text" placeholder="Name" onChange={this.nameChangedHandler} value={this.state.name} />
+				<input type="number" placeholder="Age" onChange={this.ageChangedHandler} />
+				<button
+					onClick={() => {
+						this.props.personAdded({
+							name: this.state.name,
+							age: this.state.age,
+							id: Math.random(),
+						});
+					}}
+				>
+					Add Person
+				</button>
+			</div>
+		);
+	}
+}
+
+export default AddPerson;
--- redux--assignment-2/src/containers/Persons.js
+++ redux--assignment-2/src/containers/Persons.js
@@ -10,12 +10,7 @@ class Persons extends Component {
 	// 	persons: [],
 	// };
 
-	personAddedHandler = () => {
-		const newPerson = {
-			id: Math.random(), // not really unique but good enough here!
-			name: 'Max',
-			age: Math.floor(Math.random() * 40),
-		};
+	personAddedHandler = newPerson => {
 		this.props.onAddPerson(newPerson);
 		// this.setState(prevState => {
 		// 	return { persons: prevState.persons.concat(newPerson) };
