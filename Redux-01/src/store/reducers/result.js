d11009b5380b05da5b684d87d3f33dc88e56fb3b
dbmarch <dbmarch@gmail.com>
2019-01-27 19:02:48 -0600

    clean up reducers

--- Redux-01/src/store/reducers/result.js
+++ Redux-01/src/store/reducers/result.js
@@ -1,18 +1,22 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
 	results: [],
 }
 
+const deleteResult = (state, action) => {
+	let newResults = state.results.slice(0)
+	newResults.splice(action.index, 1)
+	return updateObject(state, { results: newResults })
+}
+
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.STORE_RESULT:
-			return { ...state, results: [...state.results, action.result] }
+			return updateObject(state, { results: [...state.results, action.result] })
 		case actionTypes.DELETE_RESULT:
-			console.log(action)
-			let newResults = state.results.slice(0)
-			newResults.splice(action.index, 1)
-			return { ...state, results: newResults }
+			return deleteResult(state, action)
 
 		default:
 			console.log('uncaught action in reducer ', action.type)
